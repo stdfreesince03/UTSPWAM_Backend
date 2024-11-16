6c3e5627bd3e240551a882fec2f30ef5066e982b
stdfreesince03 <winatatristan04@gmail.com>
2024-11-16 14:55:22 +0700

    jwt middleware auth

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -7,8 +7,10 @@ export async function authenticateJWT(req, res, next) {
         return res.status(401).json({ error: 'Token not found' });
     }
     try {
+        console.log('jtw authenticated');
         const decoded = jsonwebtoken.verify(token, process.env.JWT_SECRET_KEY);
         req.user = { id: decoded.id, role: decoded.role };  // Attach `id` and `role` to `req.user`
+        console.log('jtw authenticated ,user = ',req.user);
         next();
     } catch (error) {
         console.error('JWT verification error:', error);
